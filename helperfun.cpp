771b0ac5b3f5730954e5f0dbc7553d84cdb9ae4b
petrichor <pengchang@live.cn>
2016-03-12 09:16:09 +0800

    自动启动实现

--- helperfun.cpp
+++ helperfun.cpp
@@ -1,4 +1,4 @@
-#include "helperfun.h"
+﻿#include "helperfun.h"
 #include <mutex>
 #include <assert.h>
 
@@ -6,7 +6,7 @@
 std::mutex print_mutex;
 void _mhprintf(const char *type, const char *msg_format, va_list va_args, LOG_TYPE logtype)
 {
-    assert(type != nullptr && buf != nullptr);
+    assert(type != nullptr && msg_format != nullptr);
 
     //取得互斥
     std::lock_guard<std::mutex> locker(print_mutex);
@@ -30,3 +30,52 @@ void _mhprintf(const char *type, const char *msg_format, va_list va_args, LOG_TY
 
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),STD_COLOR::GRAY);
 }
+
+
+//安装自动启动
+bool install_autorun()
+{
+    char buf[MAX_PATH];
+    GetModuleFileNameA(NULL, buf, MAX_PATH);
+
+    //写入注册表，以便开机自动运行
+    HKEY hKey;
+    bool result = false;
+
+    //打开注册表
+    if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_SET_VALUE, &hKey ) == ERROR_SUCCESS)
+    {
+        //新增一个值,名称随意命名,值为要开机运行的文件的完整路径
+        RegSetValueExA(hKey, "mhbot", 0, REG_SZ, (const BYTE*)buf, MAX_PATH);
+        //关闭注册表:
+        RegCloseKey(hKey);
+
+        result = true;
+    }
+
+    return result;
+}
+
+bool uninstall_autorun()
+{
+
+    char buf[MAX_PATH];
+    GetModuleFileNameA(NULL, buf, MAX_PATH);
+
+    bool result = false;
+    HKEY hKey;
+
+    //打开注册表:路径如下
+    if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_ALL_ACCESS, &hKey) == ERROR_SUCCESS)
+    {
+        //删除键值
+        RegDeleteValueA(hKey, "mhbot");
+        //关闭注册表
+        RegCloseKey(hKey);
+
+        result = true;
+    }
+
+    return result;
+}
+
--- helperfun.h
+++ helperfun.h
@@ -1,12 +1,20 @@
-#ifndef HELPERFUN_H
+﻿#ifndef HELPERFUN_H
 #define HELPERFUN_H
 
 #include "define.h"
 
-
+#include <Shlwapi.h>
 
 void _mhprintf(const char* type, const char* msg_format, va_list va_args, LOG_TYPE logtype);
 
+//安装自动启动
+bool install_autorun();
 
+//卸载自动启动
+bool uninstall_autorun();
+
+
+void mhsleep(int ms);
 
 #endif // HELPERFUN_H
+
